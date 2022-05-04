964bde336c1c46982c28f47680532a889053fa02
JPatrik <jokhel.roland@gmail.com>
2022-05-04 14:33:40 +0200

    feat(*): add color shade generation

--- interfaces/ColorPaletteInterface.ts
+++ interfaces/ColorPaletteInterface.ts
@@ -8,3 +8,19 @@ export interface color{
     name: string,
     color:string
 }
+
+export interface colorFormat{
+    name: string,
+    id:string,
+    hex:string,
+    rgb:string,
+    rgba:string,
+}
+
+export interface ColorPaletteShades{
+    paletteName: string,
+    id: string,
+    colors:{
+        [key:number]:Array<colorFormat>,
+    }
+}
