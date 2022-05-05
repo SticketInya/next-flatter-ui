90a2bc4d57323a111c006948dcabdf72514aae47
JPatrik <jokhel.roland@gmail.com>
2022-05-05 12:49:56 +0200

    fix(index.tsx): now uses colorPalettes context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next';
+import { useContext } from 'react';
 import HomeNav from '../components/HomeNav/HomeNav';
 import PaletteList from '../components/PaletteList/PaletteList';
 import defaultPalettes from '../constants/defaultPalettes';
+import { ColorPalettesContext } from '../contexts/ColorPalettes.context';
 
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
+    const { allPalettes } = useContext(ColorPalettesContext);
     return (
         <div className={styles.container}>
             <HomeNav />
-            <PaletteList palettes={defaultPalettes} />
+            <PaletteList palettes={allPalettes} />
         </div>
     );
 };
