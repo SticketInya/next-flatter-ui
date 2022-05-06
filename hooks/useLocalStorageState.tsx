5cf04489f8d0a7c8c6a26fdd5799162a0ff3a840
JPatrik <jokhel.roland@gmail.com>
2022-05-06 10:17:59 +0200

    fix(useLocalStorageState.tsx): fixed problems with hydration

--- hooks/useLocalStorageState.tsx
+++ hooks/useLocalStorageState.tsx
@@ -1,21 +1,19 @@
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import useDidMountEffect from './useDidMountEffect';
 
 export default function useLocalStorageState(key: string, initialValue: any) {
-    const [state, setState] = useState(getInitalState());
+    const [state, setState] = useState(initialValue);
 
-    function getInitalState() {
-        try {
-            return JSON.parse(
-                localStorage.getItem(key) || String(initialValue),
-            );
-        } catch (err) {
-            return initialValue;
-        }
-    }
-
-    useEffect(() => {
+    useDidMountEffect(() => {
         localStorage.setItem(key, JSON.stringify(state));
     }, [key, state]);
+
+    useEffect(() => {
+        const data = localStorage.getItem(key);
+        if (data !== null) {
+            setState(JSON.parse(data));
+        }
+    }, []);
 
     return [state, setState] as const;
 }
